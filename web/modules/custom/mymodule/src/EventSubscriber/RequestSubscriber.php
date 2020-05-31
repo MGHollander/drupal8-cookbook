618d2cff2a1cc68f1727685895d4d31a9cf333a1
Marc Hollander <marchollander@gmail.com>
2020-05-31 10:58:03 +0200

    Add dependency injection to the event subscriber

--- web/modules/custom/mymodule/src/EventSubscriber/RequestSubscriber.php
+++ web/modules/custom/mymodule/src/EventSubscriber/RequestSubscriber.php
@@ -3,12 +3,41 @@
 namespace Drupal\mymodule\EventSubscriber;
 
 use Drupal\Core\Url;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Session\AccountProxyInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class RequestSubscriber implements EventSubscriberInterface {
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Account proxy.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $accountProxy;
+
+  /**
+   * Creates a new RequestSubscriber object.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   * @param \Drupal\Core\Session\AccountProxyInterface $account_proxy
+   *   The current user.
+   */
+  public function __construct(RouteMatchInterface $route_match, AccountProxyInterface $account_proxy) {
+    $this->routeMatch = $route_match;
+    $this->accountProxy = $account_proxy;
+  }
+
   /**
    * Redirects all anonymous users to the login page.
    *
@@ -17,11 +46,11 @@ class RequestSubscriber implements EventSubscriberInterface {
    */
   public function doAnonymousRedirect(GetResponseEvent $event) {
     // Make sure we are not on the user login route.
-    if (\Drupal::routeMatch()->getRouteName() == 'user.login') {
+    if ($this->routeMatch->getRouteName() == 'user.login') {
       return;
     }
     // Check if the current user is logged in.
-    if (\Drupal::currentUser()->isAnonymous()) {
+    if ($this->accountProxy->isAnonymous()) {
       // If they are not logged in, create a redirect response.
       $url = Url::fromRoute('user.login')->toString();
       $redirect = new RedirectResponse($url);
